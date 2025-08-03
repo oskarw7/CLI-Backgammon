56b3058379c1b7cea6b2905c0810ffe3a8556fe7
oskarw7 <oskarwisniewski2004@gmail.com>
2025-08-03 14:14:25 +0200

    feat: rolling a dice, choosing who starts

--- src/game.rs
+++ src/game.rs
@@ -5,13 +5,17 @@ use crossterm::{
     execute,
     terminal::{self, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::io::{Write, stdout};
+use rand::Rng;
+use std::{
+    fmt::format,
+    io::{Write, stdout},
+};
 
-const PLAYER_1: char = 'w';
-const PLAYER_2: char = 'b';
-const MENU_LINE_NUMBDER: u16 = 20;
-const PROMPT_LINE_NUMBER: u16 = 21;
-const INPUT_LINE_NUMBER: u16 = 22;
+const WHITE: char = 'w';
+const BLACK: char = 'b';
+const MENU_LINE_NUMBDER: u16 = 17;
+const PROMPT_LINE_NUMBER: u16 = 18;
+const INPUT_LINE_NUMBER: u16 = 19;
 
 #[derive(Debug)]
 pub struct Game {
@@ -53,7 +57,7 @@ impl Game {
                 0,
                 2,
             ], // white takes 1-15, black takes 16-30
-            turn: PLAYER_1, // rust doesn't tolerate uninitialized fields, needed
+            turn: WHITE, // rust doesn't tolerate uninitialized fields, needed
             roll_result: Vec::new(),
             is_running: true,
         }
@@ -70,6 +74,11 @@ impl Game {
         self.board[destination - 1] += 1; // TODO: add capture logic
     }
 
+    fn roll() -> u8 {
+        let mut rng = rand::rng();
+        rng.random_range(1..=6)
+    }
+
     fn draw_checker(&self, index: usize) {
         if self.board[index] <= 15 {
             print!("●");
@@ -78,7 +87,7 @@ impl Game {
         }
     }
 
-    fn draw_empty_field(&self, i: usize) {
+    fn draw_empty_field(i: usize) {
         for j in 0..3 {
             if i < 12 {
                 move_cursor(((11 - i) * 5) as u16, (15 - j) as u16);
@@ -89,18 +98,18 @@ impl Game {
         }
     }
 
-    fn clear_board(&self) {
-        for i in 0..20 {
+    fn clear_board() {
+        for i in 0..15 {
             clear_line(i);
         }
     }
 
     fn draw_board(&self) {
-        self.clear_board();
+        clear_screen();
         for i in 0..self.board.len() {
             let mut checker_count = self.board[i];
             if checker_count == 0 {
-                self.draw_empty_field(i);
+                Game::draw_empty_field(i);
                 continue;
             }
             if checker_count > 15 {
@@ -124,7 +133,7 @@ impl Game {
     }
 
     fn get_number(&mut self, mode: &str) -> Option<u8> {
-        print_message(0, MENU_LINE_NUMBDER, "Q)uit, ESC - reset selection");
+        print_message(0, MENU_LINE_NUMBDER, "ESC - reset selection");
         let prompt = format!("Enter {mode} number:");
         print_message(0, PROMPT_LINE_NUMBER, &prompt);
 
@@ -162,7 +171,6 @@ impl Game {
                             println!("{}", input);
                         }
                         KeyCode::Esc => break,
-                        KeyCode::Char('q') => self.quit(),
                         _ => {}
                     }
                 }
@@ -171,24 +179,74 @@ impl Game {
         None
     }
 
-    pub fn play(&mut self) {
-        while self.is_running {
+    fn choose_who_starts(&mut self) {
+        let mut rolls_count = 0;
+        while rolls_count < 2 {
             self.draw();
-            print_message(0, MENU_LINE_NUMBDER, "R)oll, Q)uit, ESC - back to menu");
-            /*
+            print_message(0, MENU_LINE_NUMBDER, "R)oll, Q)uit");
+            if rolls_count == 0 {
+                print_message(0, PROMPT_LINE_NUMBER, "White rolls:");
+            } else {
+                print_message(0, PROMPT_LINE_NUMBER, "Black rolls:");
+            }
             if let Ok(event) = read() {
                 if let Event::Key(key_event) = event {
                     match key_event.code {
-                        KeyCode::Esc => return,
-                        KeyCode::Char('q') => self.quit(),
+                        KeyCode::Char('r') => {
+                            rolls_count += 1;
+                            let dice = Game::roll();
+                            self.roll_result.push(dice);
+                            let str_dice = dice.to_string();
+                            print_temp_message(0, INPUT_LINE_NUMBER, &str_dice, 1000);
+                        }
+                        KeyCode::Char('q') => {
+                            self.quit();
+                            return;
+                        },
                         _ => {}
                     }
                 }
             }
-            */
-            if let Some(source) = self.get_number("source") {
-                if let Some(destination) = self.get_number("destination") {
-                    self.move_checker(source as usize, destination as usize);
+            if rolls_count == 2 && self.roll_result[0] == self.roll_result[1] {
+                self.roll_result.clear();
+                rolls_count = 0;
+                print_temp_message(0, INPUT_LINE_NUMBER, "Tie", 1000);
+            }
+        }
+        if self.roll_result[0] > self.roll_result[1] {
+            self.turn = WHITE;
+            print_temp_message(0, INPUT_LINE_NUMBER, "White starts", 1000);
+        } else {
+            self.turn = BLACK;
+            print_temp_message(0, INPUT_LINE_NUMBER, "Black starts", 1000);
+        }
+        self.roll_result.clear();
+    }
+
+    fn play(&mut self) {
+        self.choose_who_starts();
+        while self.is_running {
+            self.draw();
+            print_message(0, MENU_LINE_NUMBDER, "R)oll, Q)uit, ESC - back to menu");
+            if let Ok(event) = read() {
+                if let Event::Key(key_event) = event {
+                    match key_event.code {
+                        KeyCode::Char('r') => {
+                            // roll logic
+                            // choosing fields
+                            loop {
+                                if let Some(source) = self.get_number("source") {
+                                    if let Some(destination) = self.get_number("destination") {
+                                        self.move_checker(source as usize, destination as usize);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        KeyCode::Esc => return,
+                        KeyCode::Char('q') => self.quit(),
+                        _ => {}
+                    }
                 }
             }
         }
